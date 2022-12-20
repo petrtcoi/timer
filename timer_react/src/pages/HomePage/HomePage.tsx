071d74ec13abc7e130fb94fa50e8236c6df4b71b
Petr Tcoi <petrtcoi@outlook.com>
2022-12-20 13:38:35 +0300

    create tiny client

--- timer_react/src/pages/HomePage/HomePage.tsx
+++ timer_react/src/pages/HomePage/HomePage.tsx
@@ -4,7 +4,11 @@ type HomePageProps = {}
 
 const HomePage: React.FC<HomePageProps> = (_props) => {
   return(
-    <div>Домашняя страница</div>
+    <div>
+      <div>Страница аукциона: "/auction/:auctionID"</div>
+      <div><a href="/_inspect" target="_blank">Инспекция всех аукционов</a></div>
+    </div>
+    
   )
 }
 
